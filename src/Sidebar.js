6ec8c3049feb18734dcb83d6ab7d83da7a62a2cd
Thanh Sang Nguyễn <sangnt@jitsinnovationlabs.com>
2022-06-29 22:05:19 +0700

    initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			<h2>Sidebar</h2>
+
+      <div className="sidebar__top">
+        <h3>BOG Programer</h3>
+        
+      </div>
+		</div>
+	);
+};
+
+export default Sidebar;
